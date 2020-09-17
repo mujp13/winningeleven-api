6cad595041008fed93ee5f1510722963bf4a40d6
Kevin Park <keunsuk413@gmail.com>
2020-09-17 10:27:18 -0500

    updated a bunch

--- migrations/03.undo.create_winning_users.sql
+++ migrations/03.undo.create_winning_users.sql
@@ -1,4 +1,4 @@
-ALTER TABLE blogful_articles
-  DROP COLUMN IF EXISTS author_id;
+ALTER TABLE winning_teams
+  DROP COLUMN IF EXISTS user_id;
 
-DROP TABLE IF EXISTS blogful_users;
+DROP TABLE IF EXISTS winning_users;
